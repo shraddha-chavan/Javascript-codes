a0561c905b4a4c67d610f32f38f4649f0b9201da
Shraddha Chavan <146915989+shraddha-chavan@users.noreply.github.com>
2025-08-24 14:53:33 +0530

    Add files via upload

---
+++ Object (day 6 )/obj.js
@@ -0,0 +1,9 @@
+const student={
+    x:"shraddha",
+    age:19,
+    cgpa:8.34,
+    isPass:true
+};
+console.log(student);
+console.log(typeof student);
+console.log(student["age"]);//here we can access one value also 
