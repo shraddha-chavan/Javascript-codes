ad7b0e16d7a50951a9ed2c783263ccb119aa4f0e
Shraddha Chavan <146915989+shraddha-chavan@users.noreply.github.com>
2025-08-24 14:53:13 +0530

    Add files via upload

---
+++ Data Types (day 5 )/dt.js
@@ -0,0 +1,40 @@
+//1) Numbers 
+let a=23;
+let b=99.09;
+console.log(a);
+console.log(b);
+console.log(typeof a);
+console.log(typeof b);
+
+
+//2) String
+let ab="shraddha";
+console.log(ab);
+console.log(typeof ab);
+
+
+//3)Boolean
+isFollow=true;
+console.log(isFollow);
+console.log(typeof isFollow);
+
+//4)Undefines
+let abc;
+console.log(abc);
+console.log(typeof abc);
+
+
+//5) Null
+let x=null;
+console.log(x);
+console.log(typeof x);
+
+//6)BigInt
+let xy=BigInt("123");
+console.log(xy);
+console.log(typeof xy);
+
+//7)Symbol
+let yz=Symbol("987");
+console.log(yz);
+console.log(typeof yz);
